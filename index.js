b986c82efa5c4955399375010bacc3fcb845a082
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2025-08-22 14:28:43 +0200

    refactor: updated cron time

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const sendWhatsAppMessages = async () => {
 
 // Schedule every Friday at 9:00 AM
 cron.schedule(
-  "24 14 * * 5",
+  "30 14 * * 5",
   () => {
     console.log("Service live", new Date().toISOString());
     sendWhatsAppMessages();
