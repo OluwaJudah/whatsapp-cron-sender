7049ef42d6487ffae1b753e0b76ffd5c5c923c36
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2025-09-05 17:17:48 +0200

    updated cron job time

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const axios = require("axios");
 
 const sendWhatsAppMessages = async () => {
   try {
+    // baby-scans-notifications
     const res = await axios.post(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/api/baby-scans-notifications`
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/appointment-reminders`
     );
     console.log("Reminders sent:", res.data);
   } catch (err) {
@@ -15,7 +16,7 @@ const sendWhatsAppMessages = async () => {
 
 // Schedule every Friday at 9:00 AM
 cron.schedule(
-  "16 11 * * 1",
+  "19 17 * * 5",
   () => {
     console.log("Service live", new Date().toISOString());
     sendWhatsAppMessages();
